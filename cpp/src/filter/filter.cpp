ccc740edf60b5faa82fb6e4e7bbca91f950786e2
Isaac Lee <legoae86@gmail.com>
2026-01-06 22:35:29 +0000

    gsajhgalkjrhjdga

--- cpp/src/filter/filter.cpp
+++ cpp/src/filter/filter.cpp
@@ -1,5 +1,62 @@
 """Reminder to self to visualize in notebook 'imports' to prevent circular calling"""
+"""
+            Summary of how the prediction step works --
+                Phase 1 in prediction step ~~ variable prediction:
 
+                    All values of the State variables start at either 0 or any default value.
+                    As the motion of the sensor data changes through time -> gryo and acc, we can
+                    estmate true acceleration by subtracting measured acc and measured gyro from acc bias 
+                    and gyro bias. Getting true acc helps attain global acceleration with respect to the world.
+                    Now perform a matrix multiplication of a rotated orientation matrix minus the gravity 
+                    since it is wrt Earth. With true acc you can now use the fundamental kinematic
+                    formulas to get a predicted velocity and position. Now in order to predict orientation, take 
+                    gyro, multiply by time step to get delta_theta and transform those euler values into quaternions
+                    to get change in quaternions. Now we have delta_q and multiply it by the current quaternions.
+
+                Phase 2 in prediciton step ~~ jacobian calculations and correlations:
+
+                    Changes in one variable may have an impact on another variable. However some variables do NOT have
+                    impact on others. The jacobian matrix comes in handy when determining how dependent certain varibles
+                    are to eachother and thus not all changes in variables may change others of the same magnitude.
+                    By getting noise jacobian and error jacobian we can compute Error covariance. Imagine two variables
+                    that are correlated to each other but we want to predict how much; by calculating the covariance,
+                    it will calculate an average point of correlation and produce a guassian distribution and on a graph,
+                    that would look like a cloud that gets more dense in the middle. Covariance would be represent how far
+                    out the cloud goes. This is then passed in the update step; continued in update step ~
+
+            VARIBLES
+
+                X: [p, q, v ab, wb] state vector
+                P: error covariance matrix
+                U: [ax ay az wx wy wz]  IMU body input vector
+                dt: time step
+        """
+
+        """
+            Summary of how Update step works --
+                Phase 1 in Update Step ~~ Sensor fusion:
+
+                    Whether it is GPS, SLAM, or altimeter data, in order to incorporate an update step it needs 
+                    some sort of global stabalizer. If you were to use a drone you might use images and process
+                    it through ORB at a lower frequency as a global stabalizer. In this ESKF we will use GPS data
+                    to work with. Remeber that guassian cloud I was reffering from earlier? Well how we use that 
+                    in the update step is by getting that 'prediction cloud' (from earlier) comparing it against 
+                    sensor data H that has it's own cloud, combine the two clouds by multiplying each point in the
+                    guassian and producing a new gaussian. That is Kalman gain. You would now update the predicted 
+                    covarience for recycling.
+
+                Phase 2 in Update Step ~~ calculate delta/error variables:
+
+                    Before talking about adding the error state variables, everytime the prediction or update step
+                    is complete we have to reset the error state to 0. This is because the small changes are never
+                    influenced by the previous calculations. Remeber that this is a local stabalizer. You can
+                    get the error values by performing a matrix multiplication with the kalman gain. After that 
+                    setting the variables is straight forward with the exception of quaternions. You have to convert
+                    euler error values into quaternions but luckily pyquaternion exists and saves a shit ton of time.
+                    Now just multiply the error quaternions to the current quaternions and then just normlise it so
+                    all values add to 1. Now just run it with each row in a data set and boom you now have a fully
+                    function Error State Kalman Filter
+        """
 #include <eskf/data/data.h>
 #include <Eigen/Dense>
 #include <Eigen/Geomentry>
